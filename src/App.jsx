e57dd9429e967b92db81facff24f61e41c9dd552
Adriana Cruz <drih1502@gmail.com>
2025-05-13 17:06:25 -0300

    correção de css

--- src/App.jsx
+++ src/App.jsx
@@ -6,7 +6,33 @@ function App() {
 
   return (
     <>
-      <div>
+      <div className="login-container">
+        <div className="title">
+          <h1>Bem Vindo</h1>
+          <p>Faça login para continuar</p>
+        </div>
+        <form id="login-form">
+          <div className="inputLogin">
+            <label htmlFor="email">Email:</label>
+            <input id="email" type="email" placeholder="E-mail" required="" />
+            <h5>Digite o email associado a sua conta</h5>
+          </div>
+          <div className="inputLogin">
+            <label htmlFor="password">Senha:</label>
+            <input id="password" type="password" placeholder="Senha" required="" />
+            <h5>Sua senha deve ter pelo menos 6 caracteres</h5>
+          </div>
+          <div className="remember-forgot">
+            <div className="remember-me">
+              <input type="checkbox" id="rememberMe" />
+              <label htmlFor="rememberMe">Lembrar-me</label>
+            </div>
+            <a href="#" id="forgotPasswordLink">Esqueceu a senha?</a>
+          </div>
+          <button type="submit">Entrar</button>
+          <p>Não tem uma conta?{" "}
+            <a href="#" id="openModal">Cadastre-se</a></p>
+        </form>
 
       </div>
     </>
